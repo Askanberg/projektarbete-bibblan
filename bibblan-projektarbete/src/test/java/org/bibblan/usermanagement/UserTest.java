c97d37cb34ae601348bf7f4fc8f51413aac4f5d2
ModerathoWinther <bjorn.mwinther@gmail.com>
2024-10-14 17:06:06 +0200

    Basic user properties and security framework

--- bibblan-projektarbete/src/test/java/org/bibblan/usermanagement/UserTest.java
+++ bibblan-projektarbete/src/test/java/org/bibblan/usermanagement/UserTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("User Management Test")
 public class UserTest {
-    private final User user = User.builder()
+    private final User user = User.builder() // Dummy-objekt utan ID eftersom det inte initialiseras utanför databas-integrationen.
             .email("anna@gmail.com")
             .userName("Anna_Book")
             .name("Anna Book")
@@ -15,35 +15,33 @@ public class UserTest {
             .build();
 
     @Test
-    public void emailInitializedCorrectly(){
-        Assertions.assertEquals(user.getEmail(), "anna@gmail.com"
-        , "Fel: Testet förväntade sig att email skulle vara \"anna@gmail.com\"");
+    public void emailInitializedCorrectly() {
+
+        Assertions.assertEquals(user.getEmail(), "anna@gmail.com",
+                "Fel: Testet förväntade sig att email skulle vara \"anna@gmail.com\"");
 
     }
 
     @Test
-    public void userNameInitializedCorrectly(){
-        Assertions.assertEquals(user.getUserName(), "Anna_Book"
-        ,"Fel: Testet förväntade sig att användarnamnet skulle vara \"Anna_Book\"");
+    public void userNameInitializedCorrectly() {
+
+        Assertions.assertEquals(user.getUserName(), "Anna_Book",
+                "Fel: Testet förväntade sig att användarnamnet skulle vara \"Anna_Book\"");
     }
 
     @Test
-    public void nameInitializedCorrectly(){
+    public void nameInitializedCorrectly() {
+
         Assertions.assertEquals(user.getName(), "Anna Book",
                 "Fel: Testet förväntade sig att namnet skulle vara \"Anna book\"");
     }
 
     @Test
-    public void passwordInitializedCorrectly(){
+    public void passwordInitializedCorrectly() {
+
         Assertions.assertEquals(user.getPassword(), "SzrqTlst_#",
                 "Fel: Testet förväntade sig att lösenordet skulle vara \"SzrqTlst_#\"");
     }
 
-    @Test
-    public void userIdGeneratesUniqueIdCorrectly(){
-
-        Assertions.assertEquals(1, user.getID(),
-                "Fel: Testet förväntade sig att ID skulle vara \"1\"");
-    }
 
 }
