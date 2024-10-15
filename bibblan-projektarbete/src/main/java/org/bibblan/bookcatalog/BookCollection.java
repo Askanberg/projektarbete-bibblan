f8cdef2cd892aebc9c2d8512456f6a076908dd6e
Askanberg <askanberg.git@gmail.com>
2024-10-15 16:38:19 +0200

    Adds method readBooksFromCsv in BookCollection, the test 'testReadBooksFromCsv' passed before refractor

--- bibblan-projektarbete/src/main/java/org/bibblan/bookcatalog/BookCollection.java
+++ bibblan-projektarbete/src/main/java/org/bibblan/bookcatalog/BookCollection.java
@@ -1,13 +1,46 @@
 package org.bibblan.bookcatalog;
 
+import org.bibblan.bookcatalog.domain.Author;
 import org.bibblan.bookcatalog.domain.Book;
+import org.bibblan.bookcatalog.domain.CoverType;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class BookCollection {
 
 
-    public Map<String, Book> readBooksFromCsv(String s) {
-        return null;
+    public Map<String, Book> readBooksFromCsv(String filePath) {
+        Map<String, Book> bookMap = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+
+                String title = values[0].trim();
+                Author author = new Author(values[1].trim(), new ArrayList<>());
+                String genre = values[2].trim();
+                String isbn = values[3].trim();
+                String publisher = values[4].trim();
+                String coverType = values[5].trim();
+
+                Book book = new Book(title, author, genre, isbn, publisher, CoverType.valueOf(coverType.toUpperCase()));
+                bookMap.put(isbn, book);
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bookMap;
+
     }
 }
