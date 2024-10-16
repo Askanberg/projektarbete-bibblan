c23bf8786e054dbe00b777e34da9b77abe0f02c7
ModerathoWinther <bjorn.mwinther@gmail.com>
2024-10-16 10:34:55 +0200

    Refraktorerade test från BCryptPasswordEncodingTest till UserSecurityTest

--- bibblan-projektarbete/src/test/java/org/bibblan/usermanagement/BCryptPasswordEncodingTest.java
+++ bibblan-projektarbete/src/test/java/org/bibblan/usermanagement/BCryptPasswordEncodingTest.java
@@ -1,30 +1,9 @@
 package org.bibblan.usermanagement;
 
-import org.bibblan.usermanagement.user.User;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Password Encoding Test")
 public class BCryptPasswordEncodingTest {
 
-    @Test
-    public void testPasswordEncoder(){
-        final User user = User.builder()
-                .userName("Test")
-                .name("Test")
-                .email("something@domain.com")
-                .password("myRawPassword")
-                .ID(101)
-                .build();
 
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-
-        assertTrue(passwordEncoder.matches(user.getPassword(), encodedPassword),
-                "Fel: Testet förväntade sig att det avkrypterade lösenordet skulle stämma överens.");
-
-    }
 }
--- bibblan-projektarbete/src/test/java/org/bibblan/usermanagement/UserSecurityTest.java
+++ bibblan-projektarbete/src/test/java/org/bibblan/usermanagement/UserSecurityTest.java
@@ -1,5 +1,31 @@
 package org.bibblan.usermanagement;
 
+import org.bibblan.usermanagement.user.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("User Security Test")
 public class UserSecurityTest {
-    
+
+    @Test
+    @DisplayName("Kryptering av lösenord fungerar.")
+    public void testPasswordEncoder(){
+        final User user = User.builder()
+                .userName("Test")
+                .name("Test")
+                .email("something@domain.com")
+                .password("myRawPassword")
+                .ID(101)
+                .build();
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+
+        assertTrue(passwordEncoder.matches(user.getPassword(), encodedPassword),
+                "Fel: Testet förväntade sig att det avkrypterade lösenordet skulle stämma överens.");
+
+    }
 }
