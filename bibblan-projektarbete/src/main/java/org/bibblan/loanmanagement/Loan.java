973a6dced1b4d59a90ded34491dfd9993356e643
josefinaferber <josefina.ferber@gmail.com>
2024-10-16 11:49:39 +0200

    Skapat test som kollar om instans av loan skapas korrekt

--- bibblan-projektarbete/src/main/java/org/bibblan/loanmanagement/Loan.java
+++ bibblan-projektarbete/src/main/java/org/bibblan/loanmanagement/Loan.java
@@ -1,8 +1,25 @@
 package org.bibblan.loanmanagement;
 
+import java.time.LocalDate;
 import org.bibblan.bookcatalog.Author;
 import org.bibblan.bookcatalog.Book;
 import org.bibblan.bookcatalog.CoverType;
 
+private LocalDate startDate;
+private LocalDate dueDate;
+private boolean returned;
+
+
+@Data
 public class Loan {
+
+    public Loan(Item book){
+        this.item = book;
+        this.startDate = LocalDate.now();
+        this.dueDate = startDate.plusDays(item.getLoanDuration());
+    }
+
+    public void returnBook(){
+        this.returned = true;
+    }
 }
--- bibblan-projektarbete/src/test/java/org/bibblan/loanmanagement/LoanTest.java
+++ bibblan-projektarbete/src/test/java/org/bibblan/loanmanagement/LoanTest.java
@@ -5,8 +5,28 @@ import org.bibblan.GeneralTestData;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 
-public class LoanTest {
-    private GeneralTestData testData = new GeneralTestData();
+
+class LoanTest {
+    private Book book;
+    private Loan loan;
+
+    @BeforeEach
+    void setUp() {
+        Author author = new Author("F. Scott Fitzgerald");
+        book = new Book("The Great Gatsby", author, "Classic", "9780743273565", "Scribner", CoverType.HARDCOVER);
+        loan = new Loan(book);
+    }
+
+    @Test
+    void testLoanCreation() { // Testar att en loan instans skapas korrekt med rätt slut och start datum
+        assertNotNull(loan);
+        assertEquals(LocalDate.now(), loan.getStartDate(), "Start date should be today's date");
+        assertEquals(LocalDate.now().plusDays(book.getLoanDuration()), loan.getDueDate(), "Due date should be correct");
+        assertFalse(loan.isReturned(), "The book should not be returned upon creation");
+    }
+
+
 
 }
