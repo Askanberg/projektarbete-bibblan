78bebcce263c803a608257f76c8c19ceb5fc3074
SSparrenholt <sixten.sparrenholt@gmail.com>
2024-10-16 11:01:01 +0200

    Adds unit test for creating a review with a mocked user and book

--- bibblan-projektarbete/src/test/java/org/bibblan/reviews/ReviewTest.java
+++ bibblan-projektarbete/src/test/java/org/bibblan/reviews/ReviewTest.java
@@ -1,7 +1,31 @@
 package org.bibblan.reviews;
 
-import org.bibblan.GeneralTestData;
+import org.bibblan.bookcatalog.Book;
+import org.bibblan.usermanagement.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 public class ReviewTest {
-    private GeneralTestData testData = new GeneralTestData();
+
+    @Mock
+    private User user;
+    @Mock
+    private Book book;
+
+
+    @Test
+    void testReviewCreationWithValidData() {
+
+        Review review = new Review(book, 5, "Good book!", user);
+
+        assertNotNull(review, "Review is null");
+        assertEquals(book, review.getBook(), "Book does not match input");
+        assertEquals(user, review.getUser(), "User does not match input");
+        assertEquals("Good book!", review.getComment(), "Comment does not match input");
+    }
+
 }
