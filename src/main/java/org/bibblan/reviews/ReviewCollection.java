b46999ab91a46e6c21280b4b528c7700cc0dc568
SSparrenholt <sixten.sparrenholt@gmail.com>
2024-10-17 10:00:37 +0200

    Adds method for adding and checking if the collections contains a certain review.

--- src/main/java/org/bibblan/reviews/ReviewCollection.java
+++ src/main/java/org/bibblan/reviews/ReviewCollection.java
@@ -1,4 +1,18 @@
 package org.bibblan.reviews;
 
+import org.bibblan.bookcatalog.domain.Book;
+import java.util.*;
+
 public class ReviewCollection {
+
+    private final Map<Book, Set<Review>> reviews = new HashMap<>();
+
+    public void addReview(Review review){
+        reviews.putIfAbsent(review.getBook(), new HashSet<>());
+        reviews.get(review.getBook()).add(review);
+    }
+
+    public boolean containsReview(Review review){
+        return reviews.get(review.getBook()).contains(review);
+    }
 }
