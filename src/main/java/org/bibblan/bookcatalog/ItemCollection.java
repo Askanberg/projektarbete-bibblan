d9c0f8a1459fdb4e29f39b418365a613b54be8f6
Askanberg <askanberg.git@gmail.com>
2024-10-21 13:25:05 +0200

    Adds functionality and test for ItemCollection, so that it can keep track of multiple copies of the same book

--- src/main/java/org/bibblan/bookcatalog/ItemCollection.java
+++ src/main/java/org/bibblan/bookcatalog/ItemCollection.java
@@ -4,27 +4,52 @@ package org.bibblan.bookcatalog;
 import lombok.Data;
 import org.bibblan.bookcatalog.domain.*;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
 
 @Data
 public class ItemCollection {
-    private Map<String, Item> itemMap = new HashMap<>();
+    private Map<String, ItemCount> itemMap = new HashMap<>();
 
     public void addItem(Item item) {
-        itemMap.put(item.getTitle(), item);
+        if (item == null) {
+            throw new IllegalArgumentException("Item is null");
+        }
+        if (itemMap.containsKey(item.getTitle())) {
+            itemMap.get(item.getTitle()).addCopy(item);
+        } else {
+            itemMap.put(item.getTitle(), new ItemCount(item, 1));
+        }
     }
 
     public void addItems(Collection<Item> item) {
         for (Item i : item) {
-            itemMap.put(i.getTitle(), i);
+            if (itemMap.containsKey(i.getTitle())) {
+                itemMap.get(i.getTitle()).addCopy(i);
+            } else {
+                itemMap.put(i.getTitle(), new ItemCount(i, 1));
+            }
         }
     }
 
-    public Item getItem(String key) {
+    public ItemCount getItemCount(String key) {
         return itemMap.get(key);
+    }
+
+    @Data
+    public static class ItemCount {
+        private String title;
+        private List<Item> totalCopies = new ArrayList<>();
+        private int count;
+
+        public ItemCount(Item item, int count) {
+            this.title = item.getTitle();
+            this.count = count;
+            totalCopies.add(item);
+        }
+
+        public void addCopy(Item item) {
+            totalCopies.add(item);
+            this.count++;
+        }
     }
 }
--- src/test/java/org/bibblan/bookcatalog/ItemCollectionTests.java
+++ src/test/java/org/bibblan/bookcatalog/ItemCollectionTests.java
@@ -4,12 +4,10 @@ import org.bibblan.bookcatalog.domain.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,17 +18,84 @@ public class ItemCollectionTests {
     public void initializeUtilities() {
         itemCollection = new ItemCollection();
     }
-
     @Test
-    void testThatAddItemsAddsCorrectlyToMap() throws IOException {
-        ItemFileReader itemFileReader = new ItemFileReader(new ItemFactory());
+    void testAddSingleItem() {
+        Book book = new Book("Educated", new Author("Tara Westover", new ArrayList<>()), "Biography", "Random House", "9780399590504", CoverType.POCKET);
 
-        ArrayList<Item> items = (ArrayList<Item>) itemFileReader.readItemsFromCsv("src/test/resources/testBooks.csv");
+        itemCollection.addItem(book);
+
+        assertEquals(book, itemCollection.getItemCount("Educated").getTotalCopies().get(0));
+    }
+    @Test
+    void testAddMultipleItems() throws IOException {
+        ItemFileReader itemFileReader = new ItemFileReader(new ItemFactory());
+        List<Item> items = itemFileReader.readItemsFromCsv("src/test/resources/testBooks.csv");
 
         itemCollection.addItems(items);
 
         for (Item i : items) {
-            assertTrue(itemCollection.getItemMap().containsValue(i));
+            assertTrue(itemCollection.getItemMap().containsKey(i.getTitle()));
         }
+    }
+
+    @Test
+    void testThatAddNullThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> itemCollection.addItem(null));
+    }
+
+    @Test
+    void testThatAddingTwoOfTheSameBooksIncreasesCount() {
+        Book bookA = new Book("Educated", new Author("Tara Westover", new ArrayList<>()), "Biography", "Random House", "9780399590504", CoverType.POCKET);
+
+        itemCollection.addItem(bookA);
+        int expectedAmountOfCopies = itemCollection.getItemMap().get(bookA.getTitle()).getCount();
+
+        itemCollection.addItem(bookA);
+
+        assertEquals(expectedAmountOfCopies + 1, itemCollection.getItemCount(bookA.getTitle()).getCount());
+    }
+
+    @Test
+    void testThatAddingTwoOfTheSameBooksButDifferentTypesIncreasesCount() {
+        Book book = new Book("Educated", new Author("Tara Westover", new ArrayList<>()), "Biography", "Random House", "9780399590504", CoverType.POCKET);
+        EBook eBook = new EBook("Educated", new Author("Tara Westover", new ArrayList<>()), "Biography", "Random House", "http://example.com/educated", "EPUB");
+
+        itemCollection.addItem(book);
+        int expectedAmountOfCopies = itemCollection.getItemMap().get(book.getTitle()).getCount();
+
+        itemCollection.addItem(eBook);
+
+        assertEquals(expectedAmountOfCopies + 1, itemCollection.getItemCount(book.getTitle()).getCount());
+    }
+    @Test
+    void testThatGetCopiesReturnsBothBooksAfterAddingTwoOfTheSameBooksButDifferentTypes() {
+        Book book = new Book("Educated", new Author("Tara Westover", new ArrayList<>()), "Biography", "Random House", "9780399590504", CoverType.POCKET);
+        EBook eBook = new EBook("Educated", new Author("Tara Westover", new ArrayList<>()), "Biography", "Random House", "http://example.com/educated", "EPUB");
+
+        List<Item> expectedItems = new ArrayList<>();
+        expectedItems.add(book);
+        expectedItems.add(eBook);
+
+        itemCollection.addItems(expectedItems);
+
+        assertEquals(expectedItems, itemCollection.getItemCount("Educated").getTotalCopies());
+    }
+
+    @Test
+    void testAddingDifferentItemTypes() throws IOException {
+        ItemFileReader itemFileReader = new ItemFileReader(new ItemFactory());
+        List<Item> ebooks =  itemFileReader.readItemsFromCsv("src/test/resources/testEBooks.csv");
+        List<Item> books =  itemFileReader.readItemsFromCsv("src/test/resources/testBooks.csv");
+
+        EBook ebook = (EBook) ebooks.get(0);
+        Book book = (Book) books.get(0);
+
+        itemCollection.addItems(ebooks);
+        itemCollection.addItems(books);
+
+
+        assertTrue(itemCollection.getItemMap().containsKey(ebook.getTitle()), "Ebook");
+        assertTrue(itemCollection.getItemMap().containsKey(book.getTitle()), "Book");
+
     }
 }
