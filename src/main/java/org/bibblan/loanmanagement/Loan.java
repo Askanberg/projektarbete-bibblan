9782c63f4bbc2cfd751fa4654eed7b758961f846
josefinaferber <josefina.ferber@gmail.com>
2024-10-25 13:31:59 +0200

    New test

--- src/main/java/org/bibblan/loanmanagement/Loan.java
+++ src/main/java/org/bibblan/loanmanagement/Loan.java
@@ -1,5 +1,49 @@
 package org.bibblan.loanmanagement;
 
 
-public class Loan {
+import java.time.LocalDate;
+
+
+import lombok.Data;
+import org.bibblan.bookcatalog.domain.Book;
+import org.bibblan.bookcatalog.domain.Item;
+
+
+@Data
+public class Loan{
+
+
+    private Book item;
+    private LocalDate startDate;
+    private LocalDate dueDate;
+    private int loanDuration;
+    private boolean returned = false;
+
+
+
+
+    public Loan(Book book) {
+        this.item = book;
+        this.startDate = LocalDate.now();
+        setLoanDuration();
+        this.dueDate = startDate.plusDays(this.loanDuration);
+    }
+
+
+
+
+    public void setLoanDuration() {
+        if ("Classics".equalsIgnoreCase(item.getGenre())) {
+            this.loanDuration = 28;
+        } else {
+            this.loanDuration = 14;
+        }
+    }
+
+
+
+
+    public void returnBook() {
+        this.returned = true;
+    }
 }
--- src/test/java/org/bibblan/loanmanagement/LoanTest.java
+++ src/test/java/org/bibblan/loanmanagement/LoanTest.java
@@ -1,27 +1,33 @@
 package org.bibblan.loanmanagement;
 
-import org.bibblan.bookcatalog.Book;
-import org.bibblan.bookcatalog.Author;
-import org.bibblan.bookcatalog.CoverType;
-import org.bibblan.loanmanagement.Loan;
+
+import org.bibblan.bookcatalog.domain.Author;
+import org.bibblan.bookcatalog.domain.Book;
+import org.bibblan.bookcatalog.domain.CoverType;
+
+import org.bibblan.bookcatalog.domain.Item;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class LoanTest {
     private Book book;
     private Loan loan;
     private Author author;
+    private List<Item> items;
+
 
     @BeforeEach
     void setUp() {
-        author = new Author("F. Scott Fitzgerald");
+        author = new Author("F. Scott Fitzgerald", items);
         book = new Book("The Great Gatsby", author, "Classic", "9780743273565", "Scribner", CoverType.HARDCOVER);
         loan = new Loan(book);
     }
+
 
     @Test
     void testLoanCreation() {
@@ -29,5 +35,11 @@ class LoanTest {
         assertEquals(LocalDate.now(), loan.getStartDate(), "Start date should be today's date");
         assertEquals(LocalDate.now().plusDays(loan.getLoanDuration()), loan.getDueDate(), "Due date should be correct");
         assertFalse(loan.isReturned(), "The book should not be returned upon creation");
+    }
+
+
+    @Test
+    void testCalculateFine_NoFineIFNotOverdue(){
+        assertEquals(0.0, loan.calculateFine(), "Fine should be 0 if loan is not overdue.");
     }
 }
