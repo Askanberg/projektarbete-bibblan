28b476a7287020f984cc5517c569bb2caff58dab
kasperostberg <109141670+kasperostberg@users.noreply.github.com>
2024-10-28 17:17:56 +0100

    restore files and fee + feetest

--- src/main/java/org/bibblan/feemanagement/Fee.java
+++ src/main/java/org/bibblan/feemanagement/Fee.java
@@ -1,4 +1,30 @@
 package org.bibblan.feemanagement;
 
+import org.bibblan.loanmanagement.Loan;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Fee {
+
+    private double totalFees;
+    private List<String> feeHistory;
+
+    public Fee() {
+        this.totalFees = 0;
+        this.feeHistory = new ArrayList<>();
+    }
+
+    public void addFee(double amount) {
+        this.totalFees += amount;
+        feeHistory.add("Added fee of " + amount + "on " + LocalDate.now());
+    }
+
+    public double calculateFine(Loan loan) {
+        return loan.calculateFine();
+    }
 }
